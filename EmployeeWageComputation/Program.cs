fbc96a2177146ae30e4087e2d93715b48d215963
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 17:19:55 +0530

    [lokendra] Refactor.wrote a code for uc2 problem statement

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,19 +8,30 @@ namespace EmployeeWageComputation
         {
             Random rand = new Random();
 
-            int is_attended = rand.Next(0, 2);
+            //constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
 
-            int attended = 1;
+            //variables
 
+            int empWage = 0;
+            int empHours = 0;
 
-            if (is_attended == attended)
+            int empCheck = rand.Next(0, 2);
+
+            if (IS_FULL_TIME == empCheck)
             {
-                Console.Out.WriteLine("Employee is present");
+                empHours = 8;
             }
             else
             {
-                Console.Out.WriteLine("Employee is absent");
+                empHours = 0;
             }
+
+            empWage = empHours * EMP_RATE_PER_HOUR;
+
+            Console.Out.WriteLine("Wage is : " + empWage);
+        }
         }
     }
-}
+
