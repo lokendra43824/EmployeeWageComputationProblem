a2075b4c37e082a83991e04d55e53f31f1bec26c
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 21:01:01 +0530

    [lokendra]Refactor.rewrote code

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,67 +4,62 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int TOTAL_WORKING_HOURS = 100;
-        //  public const int TOTAL_WORKING_DAYS = 20;
-        //  public const int WAGE_PER_HOUR = 20;
+        //constants
+
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int TOTAL_WORKING_HOURS = 100;
+        const int TOTAL_WORKING_DAYS = 20;
+        const int WAGE_PER_HOUR = 20;
 
 
-
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("Total wage  is  :" + MonthlyWage("CAP", 20, 20));
-            Console.WriteLine("Total wage  is :" + MonthlyWage("gem", 10, 10));
-
-
-
-
-
-        }
-
-
-        public static int EmpHours()
+        public static int CalculateWage()
         {
             Random rand = new Random();
-            int type = rand.Next(0, 3);
-            int hours = 0;
-            switch (type)
-            {
-                case IS_FULL_TIME:
-                    hours = 8;
-                    return hours;
-                //break;
-                case IS_PART_TIME:
-                    hours = 4;
-                    return hours;
-                //   break;
-                default:
-                    hours = 0;
-                    return hours;
-                    // break;
 
-            }
-        }
-        public static int MonthlyWage(String companyName, int TOTAL_WORKING_DAYS, int WAGE_PER_HOUR)
-        {
+
+            //variable
+
             int numberOfHours = 0;
             int workingDays = 0;
             int total_wage = 0;
-            int total_workingHrs = 0;
-            while (total_workingHrs <= 100 && workingDays < 20)
+
+
+            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
             {
-                numberOfHours = EmpHours();
+                int type = rand.Next(0, 3);
+                int hours = 0;
+                switch (type)
+                {
+                    case IS_FULL_TIME:
+                        hours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        hours = 4;
+                        break;
+                    default:
+                        hours = 0;
+                        break;
+
+                }
+
+                numberOfHours += hours;
+                Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
 
                 workingDays++;
 
 
-                //Console.WriteLine("Day " + workingDays + " hours " + numberOfHours);
-                total_workingHrs += numberOfHours;
-                total_wage = total_wage + (numberOfHours) * WAGE_PER_HOUR;
             }
+
+            total_wage = numberOfHours * WAGE_PER_HOUR;
+
             return total_wage;
+        }
+        static void Main(string[] args)
+        {
+
+            int total_wage = CalculateWage();
+            Console.Out.WriteLine("Total wage of the month :" + total_wage);
         }
 
 
