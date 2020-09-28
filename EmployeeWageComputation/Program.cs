7dd19cb2cbbb435d33bd8f45c49251fa6135b52f
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 17:36:37 +0530

    [lokendra]Refactor.added a code for calculating wages for a month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,40 +7,36 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Random rand = new Random();
-            // contants
+
+            //constants
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
+            const int MONTHLY_DAYS = 20;
             int wage = 20;
-
 
             //variable
             int type = rand.Next(0, 3);
             int hours = 0;
             int total_wage = 0;
-            String empType = "";
 
             switch (type)
             {
                 case IS_FULL_TIME:
                     hours = 8;
-                    empType = "Full_Time";
                     break;
                 case IS_PART_TIME:
                     hours = 4;
-                    empType = "Part_Time";
                     break;
                 default:
                     hours = 0;
-                    empType = "No";
                     break;
             }
 
 
+            total_wage = hours * wage * MONTHLY_DAYS;
 
-            total_wage = hours * wage;
 
-
-            Console.Out.WriteLine(empType + " employee wage: " + total_wage);
+            Console.Out.WriteLine("employee wage per month: " + total_wage);
         }
         }
     }
