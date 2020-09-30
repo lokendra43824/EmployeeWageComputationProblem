26d269c75999c540c8e6292a30342b13812f4f01
Lokendra <lokilokendra12345@gmail.com>
2020-09-30 23:24:44 +0530

    [lokendra] added a code for uc-13

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,53 +5,50 @@ namespace EmployeeWageComputation
     class Program
     {
 
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+
+
         static void Main(string[] args)
         {
-            Random rand = new Random();
+            //Variables
+            Double empRatePerHour = 0;
+            Double workingHrs = 0;
+            Double numOfWorkingDays = 0;
 
-            //constants
+            Random random = new Random();
+            int empType = random.Next(1, 3);
 
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int TOTAL_WORKING_HOURS = 100;
-            const int TOTAL_WORKING_DAYS = 20;
-            const int WAGE_PER_HOUR = 20;
-
-            //variable
-
-            int numberOfHours = 0;
-            int workingDays = 0;
-            int total_wage = 0;
-
-
-            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
+            switch (empType)
             {
-                int type = rand.Next(0, 3);
-                int hours = 0;
-                switch (type)
-                {
-                    case IS_FULL_TIME:
-                        hours = 8;
-                        break;
-                    case IS_PART_TIME:
-                        hours = 4;
-                        break;
-                    default:
-                        hours = 0;
-                        break;
+                case IS_FULL_TIME:
 
-                }
+                    empRatePerHour = 120;
+                    workingHrs = 8;
+                    numOfWorkingDays = 20;
 
-                numberOfHours += hours;
+                    Console.WriteLine("Full Time Employee Details in following companies");
+                    break;
 
-                workingDays++;
+                case IS_PART_TIME:
 
-                Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
+                    empRatePerHour = 80;
+                    workingHrs = 5;
+                    numOfWorkingDays = 20;
+                    Console.WriteLine("Part Time Employee Details in following companies");
+                    break;
+
+                default:
+                    break;
             }
 
-            total_wage = numberOfHours * WAGE_PER_HOUR;
+            EmpWageBuilder apple = new EmpWageBuilder();
+            apple.addCompanyWage("Apple", empRatePerHour, numOfWorkingDays, workingHrs);
+            Console.WriteLine("Total wage in Apple: " + apple.getTotalWage("Apple"));
 
-            Console.Out.WriteLine("Total wage of the month :" + total_wage);
+            EmpWageBuilder google = new EmpWageBuilder();
+            google.addCompanyWage("Google", empRatePerHour, numOfWorkingDays, workingHrs);
+            Console.WriteLine("Total wage in google : " + google.getTotalWage("Google"));
 
         }
     }
