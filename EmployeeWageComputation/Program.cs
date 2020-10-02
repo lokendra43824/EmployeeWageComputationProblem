a5111b0b34d8e19f898c2c53433ad1f7626431ca
Lokendra <lokilokendra12345@gmail.com>
2020-10-02 17:02:53 +0530

    [lokenda] updated a code for uc-14

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,57 +1,67 @@
-﻿using System;
+﻿using EmployeeWage;
+using System;
+using System.Collections.Generic;
 
-namespace EmployeeWageComputation
+namespace Employee_Wage_calc_Problem
 {
     class Program
     {
+        //constants
+
         static void Main(string[] args)
         {
-            Random rand = new Random();
 
-            //constants
+            //Constants
 
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int TOTAL_WORKING_HOURS = 100;
-            const int TOTAL_WORKING_DAYS = 20;
-            const int WAGE_PER_HOUR = 20;
+            const int ADDCompany = 1;
+            const int GetCompWage = 2;
 
-            //variable
+            EmpWageBuilder ew = new EmpWageBuilder();
 
-            int numberOfHours = 0;
-            int workingDays = 0;
-            int total_wage = 0;
+            bool loop = true;
 
-
-            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
+            while (loop)
             {
-                int type = rand.Next(0, 3);
-                int hours = 0;
-                switch (type)
+                Console.WriteLine("1. Add Company and Caluclate Wage\n2.Get total monthly wage of a company \n3.Exit");
+                int choose = Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
                 {
-                    case IS_FULL_TIME:
-                        hours = 8;
+
+                    case ADDCompany:
+
+                        Company company = ew.AddCompany("Apple", 120, 20, 100);
+                        ew.CalculateWage(company);
+
+                        company = ew.AddCompany("Google", 100, 25, 150);
+                        ew.CalculateWage(company);
                         break;
-                    case IS_PART_TIME:
-                        hours = 4;
-                        break;
-                    default:
-                        hours = 0;
+                    case GetCompWage:
+
+                        Console.Out.WriteLine("Enter the company to get total wage");
+                        String name = Console.ReadLine();
+                        double monthlyWage = ew.GetTotalWage(name);
+                        if (monthlyWage > 0)
+                        {
+                            Console.Out.WriteLine(name + "'s Monthly total wage is: " + monthlyWage);
+                        }
+                        else
+                        {
+                            Console.Out.WriteLine("Invalid Company Name");
+                        }
                         break;
 
+                    default:
+                        loop = false;
+                        break;
                 }
 
-                numberOfHours += hours;
+                Console.Out.WriteLine("\n**************************************\n");
 
-                workingDays++;
-
-                Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
             }
 
-            total_wage = numberOfHours * WAGE_PER_HOUR;
 
-            Console.Out.WriteLine("Total wage of the month :" + total_wage);
+
         }
     }
 }
-
