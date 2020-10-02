02a4ecfcafc8c02702c85c9577e697eb3447af4a
Lokendra <lokilokendra12345@gmail.com>
2020-10-02 16:41:15 +0530

    [lokenda] updated a code for uc-12

---
+++ EmployeeWageComputation/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class Company
+    {
+        //getters and setters for the instance variables
+
+        public String CompName { get; set; }
+        public double WagePerHour { get; set; }
+        public int TotalWorkingDays { get; set; }
+        public int TotalWorkingHours { get; set; }
+
+        public double MonthlyTotalWage { get; set; }
+
+
+        //company constructor
+        public Company(String compName, double wagePerHour, int totalWorkingDays, int totalWorkingHours)
+        {
+            this.CompName = compName;
+            this.WagePerHour = wagePerHour;
+            this.TotalWorkingDays = totalWorkingDays;
+            this.TotalWorkingHours = totalWorkingHours;
+            this.MonthlyTotalWage = 0;
+
+            Console.WriteLine("\nCompany " + this.CompName + " Created\n");
+
+        }
+    }
+}
---
+++ EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+
+    class EmpWageBuilder : IEmpWageCal
+    {
+        //constants
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+
+        List<Company> listOfCompnies = new List<Company>();
+
+        public EmpWageBuilder()
+        {
+
+        }
+
+        public Company AddCompany(String compName, double wagePerHour, int totalWorkingDays, int totalWorkHours)
+        {
+            Company company = new Company(compName, wagePerHour, totalWorkingDays, totalWorkHours);
+            listOfCompnies.Add(company);
+            return company;
+        }
+
+
+        public void CalculateWage(Company company)
+        {
+            Random rand = new Random();
+
+            //variables
+
+            int numberOfHours = 0;
+            int workingDays = 1;
+            double totalWage = 0;
+
+
+            // run till the number of hours or workingdays reached in respective company
+
+            while (numberOfHours <= company.TotalWorkingHours && workingDays <= company.TotalWorkingDays)
+            {
+
+                int type = rand.Next(0, 3);
+                int hours = 0;
+
+                // switch to type of Employee
+
+                switch (type)
+                {
+                    case IS_FULL_TIME:
+                        hours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        hours = 4;
+                        break;
+                    default:
+                        hours = 0;
+                        break;
+
+                }
+
+                // add the hours to total hrs
+
+                numberOfHours += hours;
+
+                Console.Out.WriteLine(company.CompName + " Day " + workingDays + " hours " + hours);
+
+                workingDays++;
+            }
+
+
+
+            totalWage = numberOfHours * company.WagePerHour;
+
+            Console.Out.WriteLine("\nTotal Wage for the month is: " + totalWage);
+
+            company.MonthlyTotalWage = totalWage;
+        }
+
+    }
+}
