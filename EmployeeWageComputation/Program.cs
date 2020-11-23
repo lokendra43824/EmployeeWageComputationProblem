916208f47474e92708dcea8db98d040639ef9435
Lokendra <lokilokendra12345@gmail.com>
2020-11-23 23:41:05 +0530

    Refactored a code to uc-8

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,27 +8,27 @@ namespace EmployeeWageComputation
 
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
-        const int TOTAL_WORKING_HOURS = 100;
-        const int TOTAL_WORKING_DAYS = 20;
-        const int WAGE_PER_HOUR = 20;
 
-
-        public static int CalculateWage()
+        public static int CalculateWage(String comp_name, int wage_per_hour, int total_working_days, int total_working_hours)
         {
             Random rand = new Random();
-
 
             //variable
 
             int numberOfHours = 0;
-            int workingDays = 0;
+            int workingDays = 1;
             int total_wage = 0;
 
+            // run till the number of hours or workingdays reached in respective company
 
-            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
+            while (numberOfHours <= total_working_hours && workingDays <= total_working_days)
             {
+
                 int type = rand.Next(0, 3);
                 int hours = 0;
+
+                // switch to type of Employee
+
                 switch (type)
                 {
                     case IS_FULL_TIME:
@@ -43,27 +43,34 @@ namespace EmployeeWageComputation
 
                 }
 
+                // add the hours to total hrs
+
                 numberOfHours += hours;
-                Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
+
+                Console.Out.WriteLine(comp_name + " Day " + workingDays + " hours " + hours);
 
                 workingDays++;
 
-
             }
 
-            total_wage = numberOfHours * WAGE_PER_HOUR;
+            total_wage = numberOfHours * wage_per_hour;
 
             return total_wage;
         }
         static void Main(string[] args)
         {
 
-            int total_wage = CalculateWage();
-            Console.Out.WriteLine("Total wage of the month :" + total_wage);
+            int total_wage = 0;
+
+            total_wage = CalculateWage("Apple", 150, 20, 150);
+
+            Console.Out.WriteLine("Apple's Total wage of the month  :" + total_wage);
+
+            total_wage = CalculateWage("Google", 125, 22, 150);
+
+            Console.Out.WriteLine("Google's Total wage of the month :" + total_wage);
+
         }
-
-
-
     }
 
 }
